bccef73be844a8724d34c9083f9cd35573f1caf8
Sakshi Yadu <sakshixtech@gmail.com>
2024-03-30 22:47:46 +0530

    Create script.sh

---
+++ NGS/Sakshi/script.sh
@@ -0,0 +1,7 @@
+### This script defines the Nextflow pipeline to perform the following steps:
+
+## Quality Control (FastQC)
+## Trimming (FastP)
+## Genome Mapping (BWA)
+## Variant Calling (bcftools)
+
