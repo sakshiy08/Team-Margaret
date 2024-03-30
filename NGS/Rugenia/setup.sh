bede115eba6af9d0fae3b803873f4d25ff4fab19
Ruth Agbeyeke <ragbeyeke@gmail.com>
2024-03-30 22:29:52 +0000

    setup and script files
    Successful upload of task 2

---
+++ NGS/Rugenia/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install FastQC
+sudo apt-get install fastqc
+
+# Install FastP
+sudo apt-get install fastp
+
+# Install BWA
+sudo apt-get install bwa
+
+# Install SAMTOOLS
+sudo apt-get install samtools
+
+# Install bcftools
+sudo apt-get install bcftools
