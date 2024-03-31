3e39c27ba6e48ddf017d0f98baed60008c07680b
Sakshi Yadu <sakshixtech@gmail.com>
2024-03-31 19:48:02 +0530

    Update script.sh

--- NGS/Sakshi/script.sh
+++ NGS/Sakshi/script.sh
@@ -1,7 +1,31 @@
-### This script defines the Nextflow pipeline to perform the following steps:
+mkdir pipeline && cd pipeline 
 
-## Quality Control (FastQC)
-## Trimming (FastP)
-## Genome Mapping (BWA)
-## Variant Calling (bcftools)
+##Download dataset (wget)
+
+wget -O forward.fastq.gz https://zenodo.org/records/10426436/files/ERR8774458_1.fastq.gz?download=1
+wget -O reverse.fastq.gz https://zenodo.org/records/10426436/files/ERR8774458_2.fastq.gz?download=1
+wget -O reference.fasta https://zenodo.org/records/10886725/files/Reference.fasta?download=1
+
+##Quality Control (FastQC) 
+
+fastqc forward.fastq.gz
+fastqc reverse.fastq.gz
+ls
+google-chrome forward_fastqc.html 
+google-chrome reverse_fastqc.html
+
+##Trimming (FastP) 
+
+fastp -i forward.fastq.gz -I reverse.fastq.gz -o trimmed_forward.fastq -O trimmed_reverse.fastq
+ls
+google-chrome fastp.html
+
+##Genome Mapping (bwa) 
+ 
+bwa index Reference.fasta 
+bwa mem -M reference.fasta trimmed_forward.fastq trimmed_reverse.fastq > mapped_reads.sam
+
+
+
+##Variant Calling (bcftools)
 
