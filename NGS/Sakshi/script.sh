38614a154f898f103829297dc182fdaca6a062a1
Sakshi Yadu <sakshixtech@gmail.com>
2024-04-05 00:33:15 +0530

    Update script.sh

--- NGS/Sakshi/script.sh
+++ NGS/Sakshi/script.sh
@@ -2,27 +2,36 @@ mkdir pipeline && cd pipeline
 
 ##Download dataset (wget)
 
-wget -O forward.fastq.gz https://zenodo.org/records/10426436/files/ERR8774458_1.fastq.gz?download=1
-wget -O reverse.fastq.gz https://zenodo.org/records/10426436/files/ERR8774458_2.fastq.gz?download=1
-wget -O reference.fasta https://zenodo.org/records/10886725/files/Reference.fasta?download=1
+wget https://raw.githubusercontent.com/josoga2/yt-dataset/main/dataset/raw_reads/reference.fasta  ##reference genome
+
+wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/ACBarrie_R1.fastq.gz
+wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/ACBarrie_R2.fastq.gz
+wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Alsen_R1.fastq.gz
+wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Alsen_R2.fastq.gz 
+wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Baxter_R1.fastq.gz 
+wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Baxter_R2.fastq.gz 
+wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Chara_R1.fastq.gz 
+wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Chara_R2.fastq.gz 
+wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Drysdale_R1.fastq.gz 
+wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Drysdale_R2.fastq.gz 
 
 ##Quality Control (FastQC) 
 
-fastqc forward.fastq.gz
-fastqc reverse.fastq.gz
-ls
-google-chrome forward_fastqc.html 
-google-chrome reverse_fastqc.html
+mkdir -p qcoutput && fastqc *.fastq.gz -o qcoutput/
 
 ##Trimming (FastP) 
 
-fastp -i forward.fastq.gz -I reverse.fastq.gz -o trimmed_forward.fastq -O trimmed_reverse.fastq
-ls
-google-chrome fastp.html
+nano trim.sh 
+#!/bin/bash
+for file in *R1.fastq.gz; do
+    fastp -i $file -I ${file/R1/R2} -o trimmed_$(basename $file .gz) -O trimmed_$(basename ${file/R1/R2} .gz)
+done
+chmod +x trim.sh
+./trim.sh 
 
 ##Genome Mapping (bwa) 
 
-bwa index Reference.fasta 
+bwa index reference.fasta 
 bwa mem -M reference.fasta trimmed_forward.fastq trimmed_reverse.fastq > mapped_reads.sam
 samtools view -S -b mapped_reads.sam > mapped_reads.bam 
 samtools sort mapped_reads.bam -o sorted_mapped_reads.bam 
