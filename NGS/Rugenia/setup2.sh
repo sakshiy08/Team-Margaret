8509ec1ba71826fc178a22072a1f7b6d99365fc5
Ruth Agbeyeke <ragbeyeke@gmail.com>
2024-04-05 01:34:57 +0100

    Setup 2
    Setup script for extension of task 2

---
+++ NGS/Rugenia/setup2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Install FastQC
+sudo apt-get install fastqc
+
+# Install FastP
+sudo apt-get install fastp
+
+# Install BWA
+sudo apt-get install bwa
+
+# Install SAMTOOLS
+sudo apt-get install samtools
+
+# Install bcftools
+sudo apt-get install bcftools
+
+# Install bbtools
+conda install agbiome::bbtools #0r
+sudo apt-get install bcftools
+
