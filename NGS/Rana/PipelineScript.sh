91493bd7eb5d338b97c70f8a6777c44f53b3d44e
Rana Sabry Ismaeil <ranaasabrii2@gmail.com>
2024-04-05 02:33:33 +0200

    Update PipelineScript.sh

--- NGS/Rana/PipelineScript.sh
+++ NGS/Rana/PipelineScript.sh
@@ -1,45 +1,76 @@
-  460  mkdir Dataset && cd Dataset
-  461  wget https://zenodo.org/records/10426436/files/ERR8774458_1.fastq.gz?download=1
-  462  wget https://zenodo.org/records/10426436/files/ERR8774458_2.fastq.gz?download=1
-  463  ls
-  464  mv 'ERR8774458_1.fastq.gz?download=1' Forward.fastq.gz && mv 'ERR8774458_2.fastq.gz?download=1' Reverse.fastq.gz
-  465  ls
-  466  gunzip Forward.fastq.gz && gunzip Reverse.fastq.gz
-  467  cd ..
-  468  mkdir Ref
-  469  cd Ref
-  470  wget https://zenodo.org/records/10886725/files/Reference.fasta?download=1
-  471  ls
-  472  mv 'Reference.fasta?download=1' Reference.fasta
-  473  cd ..
-  474  mkdir QC_Reports
-  475  fastqc Dataset/Forward.fastq -o QC_Reports/
-  476  fastqc Dataset/Reverse.fastq -o QC_Reports/
-  477  multiqc QC_Reports/
-  478  mkdir Trimmed
-  479  ls Dataset/
-  480  fastp -i Dataset/Forward.fastq -I Dataet/Reverse.fastq -o Trimmed/Trimmed_Forward.fastq -O Trimmed/Trimmed_Reverse.fastq
-  481  fastp -i Dataset/Forward.fastq -I Dataset/Reverse.fastq -o Trimmed/Trimmed_Forward.fastq -O Trimmed/Trimmed_Reverse.fastq
-  482  cd Trimmed
-  483  ls
-  484  fastqc Trimmed_Forward.fastq
-  485  fastqc Trimmed_Reverse.fastq
-  486  cd ..
-  487  ls
-  488  cd Ref
-  489  ls
-  490  bwa index Reference.fasta 
-  491  cd ..
-  492  ls Trimmed/
-  493  bwa mem Ref/Reference.fasta Trimmed/Trimmed_Forward.fastq Trimmed/Trimmed_Reverse.fastq > aligned.sam
-  494  samtools view -S -b aligned.sam > aligned.bam
-  495  samtools sort -o aligned.sorted.bam aligned.bam
-  496  samtools flagstat aligned.sorted.bam
-  497  mkdir results && cd results
-  498  mkdir bcf
-  499  cd ..
-  500  bcftools mpileup -O b -o results/bcf/SRR2584866_raw.bcf -f Ref/Reference.fasta aligned.sorted.bam
-  501  bcftools call --ploidy 1 -m -v -o results/vcf/SRR2584866_variants.vcf results/bcf/SRR2584866_raw.bcf
-  502  vcfutils.pl varFilter results/vcf/SRR2584866_variants.vcf  > results/vcf/SRR2584866_final_variants.vcf
-  503  less -S results/vcf/SRR2584866_final_variants.vcf
-  504  history > Rana_NGS.sh
+#Setting up the workspace
+#downloading the datasets
+
+$ mkdir dataset
+$ cd dataset
+$ mkdir raw_data && cd raw_data
+$ wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/ACBarrie_R1.fastq.gz
+$ wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/ACBarrie_R2.fastq.gz
+$ wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Alsen_R1.fastq.gz
+$ wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Alsen_R2.fastq.gz
+$ wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Baxter_R1.fastq.gz
+$ wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Baxter_R2.fastq.gz
+$ wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Chara_R1.fastq.gz
+$ wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Chara_R2.fastq.gz
+$ wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Drysdale_R1.fastq.gz
+$ wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Drysdale_R2.fastq.gz
+$ cd ..
+$ ls
+$ cd ..
+$ ls
+$ mkdir QCR
+$ ls
+$ ls raw_data/
+
+#Quality Control Fastqc
+$ fastqc raw_data/*.fastq.gz -o QCR
+
+#MultiQC
+$ multiqc QCR/
+
+#Trimming with Fastp (If needed )
+$ vim trimming.sh
+$ cp trimming.sh raw_data/
+
+$ cd raw_data
+$ ls
+$ bash trimming.sh 
+$ mv qc_reads/ Trimmed
+
+#Repairing
+$ mkdir repaired
+$ for f1 in *_R1.fastq.gz; do
+  f2="${f1%_R1.fastq.gz}_R2.fastq.gz"
+  repairdir="repaired"  # Define the directory name here
+  mkdir -p "$repairdir"  # Create the "repaired" directory if it doesn't exist
+  repair.sh in1="$f1" in2="$f2" out1="$repairdir/${f1%.gz}_repaired_R1.fastq.gz" out2="$repairdir/${f2%.gz}_repaired_R2.fastq.gz" outs="$repairdir/${f1%.gz}_singletons.fastq.gz" repair=t
+done
+
+
+#Genome Mapping 
+## indexing
+$ cd ..
+$ bwa index Ref/reference.fasta
+
+## mem and from SAM to BAM using Samtools
+for f1 in *_R1.fastq.gz; do
+  f2="${f1%_R1.fastq.gz}_R2.fastq.gz"
+  bwa mem ../Ref/reference.fasta "$f1" "$f2" | samtools view -Sb - > "${f1%.gz}.bam"
+done
+
+## sort
+$ for f in *.bam; do
+  sorted_bam="${f%.bam}.sorted.bam"  # Create the sorted BAM filename
+  samtools sort "$f" "$sorted_bam"
+done
+
+## statistical report
+for f in *.bam; do
+  samtools flagstat "$f"
+done
+
+## Variant calling using bcftools
+$ for file in bam/*.sorted.bam; do
+    bcftools mpileup -Ou -f download/reference.fasta "$file" | bcftools call -mv -Ov -o "vcf/$(basename "${file/.sorted.bam/.vcf}")"
+done
+
