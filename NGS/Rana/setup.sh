110b6ecb6081d58dc9057d273503590a5efe32a0
Rana Sabry Ismaeil <ranaasabrii2@gmail.com>
2024-04-05 02:37:01 +0200

    Update setup.sh

--- NGS/Rana/setup.sh
+++ NGS/Rana/setup.sh
@@ -1,7 +1,8 @@
 #!/usrs/bin/bash
 
 #Adding some channels
-conda config --add channels bioconda &&  conda config --add channels conda-forge
+conda config --add channels bioconda &&  conda config --add channels conda-forge && conda config --add channels agbiome
+
 
 #Fastqc
 conda install -c bioconda fastqc
@@ -12,6 +13,8 @@ conda install -c bioconda multiqc
 #Fastp
 conda install -c bioconda fastp
 
+#bbtools
+conda install agbiome::bbtools
 #BWA
 conda install -c bioconda bwa
 
