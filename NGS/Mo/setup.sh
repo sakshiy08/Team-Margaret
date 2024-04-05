4cadbaeb37631c1a00db998e31e9a19d9455df71
Alabi MosopefOluwa <70078984+mosopefoluwa2@users.noreply.github.com>
2024-04-05 22:30:20 +0100

    Add files via upload

---
+++ NGS/Mo/setup.sh
@@ -0,0 +1,9 @@
+Here is an installation guided for an ngs pipeline
+
+38  sudo apt update
+39  sudo apt install fastqc # to install fastqc
+40  sudo apt install fastp # to install fastp
+41  sudo apt install bwa # to install bwa
+42  sudo apt install samtools #to install samtools
+43  sudo apt install bcftools #to install bcftools
+
