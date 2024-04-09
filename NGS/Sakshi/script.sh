9f36780d8bfd33235e6dbb61842b77ae9ed8ee03
Sakshi Yadu <sakshixtech@gmail.com>
2024-04-09 09:53:37 +0530

    Update script.sh

--- NGS/Sakshi/script.sh
+++ NGS/Sakshi/script.sh
@@ -15,6 +15,8 @@ wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Chara_R1.f
 wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Chara_R2.fastq.gz 
 wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Drysdale_R1.fastq.gz 
 wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Drysdale_R2.fastq.gz 
+wget https://zenodo.org/records/10426436/files/ERR8774458_1.fastq.gz
+wget https://zenodo.org/records/10426436/files/ERR8774458_2.fastq.gz
 
 ##Quality Control (FastQC) 
 
